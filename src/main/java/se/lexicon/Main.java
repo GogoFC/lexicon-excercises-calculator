9b6e1412ed7da285e3f5336577b7a91af1fd84c1
Goran Muminovic <goranmuminovic@gmail.com>
2023-11-15 10:02:37 +0100

    added other methods

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -9,17 +9,20 @@ public class Main {
 
 
 
-        double sum = addition(number1,number2);
+        double sumOfNumbers = addition(number1,number2);
         double multipliedNumbers = multiply(number1, number2);
+        double subtractedNumbers = subtraction(number1, number2);
+        double dividedNumbers = division(number1, number2)
 
-        double division = number2 / number1;
-        double subtraction = number1 - number2;
+
+        //double division = number2 / number1;
+        //double subtraction = number1 - number2;
         //double multiplication = number1 * number2;
 
-        System.out.println("The sum of numbers " + number1 + " and " + number2 + " = " +sum);
+        System.out.println("The sum of numbers " + number1 + " and " + number2 + " = " +sumOfNumbers);
         System.out.println(number1 + " times " + number2 + " is " +multipliedNumbers);
-        System.out.println(number1 + " divided by " + number2 + " = " +division);
-        System.out.println(number1 + " minus " + number2 + " is " +subtraction);
+        System.out.println(number1 + " divided by " + number2 + " = " +dividedNumbers);
+        System.out.println(number1 + " minus " + number2 + " is " +subtractedNumbers);
     }
     static double addition(double num1, double num2){
         double result = num1 + num2;
@@ -28,6 +31,16 @@ public class Main {
 
     static double multiply(double num1, double num2){
         double result = num1 * num2;
+        return result;
+    }
+
+    static double subtraction(double num1, double num2){
+        double result = num1 - num2;
+        return result;
+    }
+
+    static double division(double num1, double num2){
+        double result = num1 / num2;
         return result;
     }
 

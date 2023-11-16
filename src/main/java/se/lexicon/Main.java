c03f8b7660bc3de180fc1e08d5ef487b3b019f0a
Goran Muminovic <goranmuminovic@gmail.com>
2023-11-16 09:54:35 +0100

    added newline instead of printing spaces

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -28,10 +28,8 @@ public class Main {
 
 
 
-            System.out.println(" ");
-            System.out.println("Choose an option and hit the Enter key");
-            System.out.println(" ");
-            System.out.println("Type 1 to ADD");
+            System.out.println("\n" + "Choose an option and hit the Enter key");
+            System.out.println("\n" + "Type 1 to ADD");
             System.out.println("Type 2 to SUBTRACT");
             System.out.println("Type 3 to MULTIPLY");
             System.out.println("Type 4 to DIVIDE");
@@ -55,34 +53,26 @@ public class Main {
 
             if (choice == 1) {
                 double sumOfNumbers = addition(number1, number2);
-                System.out.println(" ");
-                System.out.println("The sum of numbers " + number1 + " and " + number2 + " = " + sumOfNumbers);
+                System.out.println( "\n" + "The sum of numbers " + number1 + " and " + number2 + " = " + sumOfNumbers);
             } else if (choice == 2) {
                 double subtractedNumbers = subtraction(number1, number2);
-                System.out.println(" ");
-                System.out.println(number1 + " minus " + number2 + " is " + subtractedNumbers);
+                System.out.println("\n" + number1 + " minus " + number2 + " is " + subtractedNumbers);
             } else if (choice == 3) {
                 double multipliedNumbers = multiply(number1, number2);
-                System.out.println(" ");
-                System.out.println(number1 + " times " + number2 + " is " + multipliedNumbers);
+                System.out.println("\n" + number1 + " times " + number2 + " is " + multipliedNumbers);
             } else if (choice == 4) {
                 double dividedNumbers = division(number1, number2);
-                System.out.println(" ");
-                System.out.println(number1 + " divided by " + number2 + " = " + dividedNumbers);
+                System.out.println("\n" + number1 + " divided by " + number2 + " = " + dividedNumbers);
             } else {
-                System.out.println(" ");
-                System.out.println("Not a valid choice. Choose 1 through 4");
+                System.out.println("\n" + "Not a valid choice. Choose 1 through 4");
 
             }
 
 
 
-            System.out.println(" ");
-            System.out.println("Would you like to Exit the program?");
-            System.out.println(" ");
-            System.out.println("Type 8 to exit");
-            System.out.println(" ");
-            System.out.println("Type any number to continue");
+            System.out.println("\n" + "Would you like to Exit the program?");
+            System.out.println("\n" + "Type 8 to exit");
+            System.out.println("\n" + "Type any number to continue");
 
             while (!numbers.hasNextDouble()) {
                 System.out.println("Not a number, re-enter");
